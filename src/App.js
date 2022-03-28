16810d75a1d164f6d0cd8b66ffdd8d766a4ff483
loganevans98 <loganevans98@gmail.com>
2022-03-27 22:43:05 -0400

    a7 Redux sp22 cleanup

--- src/App.js
+++ src/App.js
@@ -11,29 +11,25 @@ import React from "react";
 
 
 function App() {
-  return (
-      <BrowserRouter>
-          <div className="container">
-              <Routes>
-                  <Route path="/">
-                      <Route path="labs"
-                             element={<Labs/>}/>
-                      <Route path="hello"
-                             element={<HelloWorld/>}/>
-                      <Route path="tuiter"
-                             element={<Tuiter />}>
-                          <Route index
-                                 element={<HomeScreen/>}/>
-                          <Route path="explore"
-                                 element={<ExploreScreen/>}/>
-                          ...
-                      </Route>
-                  </Route>
-              </Routes>
-          </div>
-      </BrowserRouter>
-
-  );
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <Routes>
+                    <Route path="/">
+                        <Route index element={<Labs />} />
+                        <Route path="labs" exact={true} element={<Labs />} />
+                        <Route path="hello" element={<HelloWorld/>}/>
+                        <Route path="tuiter" element={<Tuiter/>}>
+                            <Route index element={<HomeScreen/>}/>
+                            <Route path="explore" element={<ExploreScreen/>}/>
+                        </Route>
+                    </Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
+
+
 export default App;
 
